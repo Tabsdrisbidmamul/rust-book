d78d10ce57ec0ba7381915d341e089dab583b01c
Idris Khan <idris.khan303@gmail.comm>
2023-08-23 18:51:03 +0100

    refactor

--- section-12-io-proj/src/main.rs
+++ section-12-io-proj/src/main.rs
@@ -1,26 +1,38 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
 
-    if args.len() <= 1 {
-        return ();
-    }
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let (query, file_path) = parse_config(&args);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
-
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read file");
 
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    (query, file_path)
+impl Config {
+    fn build(args: &Vec<String>) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
