3b92129a435709195062730f6836a1311c0bae6f
Idris Khan <idris.khan303@gmail.comm>
2023-08-24 18:28:02 +0100

    refactor to lib

--- section-12-io-proj/src/main.rs
+++ section-12-io-proj/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use section_12_io_proj::{run, Config};
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
@@ -13,26 +14,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read file");
-
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &Vec<String>) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
