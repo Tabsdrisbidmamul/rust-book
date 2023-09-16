266eba8ce346f18648b4d1003b9a18d66e222089
Idris Khan <idris.khan303@gmail.comm>
2023-09-16 14:40:23 +0100

    drops and destructors covered

---
+++ section-15-3-drop/src/main.rs
@@ -0,0 +1,24 @@
+#[derive(Debug)]
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data)
+    }
+}
+
+fn main() {
+    let c = CustomSmartPointer {
+        data: String::from("My stuff"),
+    };
+
+    let d = CustomSmartPointer {
+        data: String::from("Other stuff"),
+    };
+
+    println!("CustomSmartPointers created");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main")
+}
